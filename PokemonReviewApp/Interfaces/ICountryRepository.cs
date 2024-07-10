64293492604cf77735ea7cd8e18ecf8526194e26
fatihademtas0 <fatihademtas@outlook.com>
2024-07-11 01:59:56 +0300

    first commit

---
+++ PokemonReviewApp/Interfaces/ICountryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        ICollection<Pokemon> GetPokemonByCountry(int countryId);
+        bool CountryExists(int countryId);
+
+    }
+}
---
+++ PokemonReviewApp/Repository/CountryRepository.cs
@@ -0,0 +1,28 @@
+﻿using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CountryRepository : ICountryRegit adpository
+    {
+        public bool CountryExists(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Country> GetCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Country GetCountry(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCountry(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
