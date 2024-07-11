de6f5219483e348b4095f4dba4b4baccfcc4fbb9
fatihademtas0 <fatihademtas@outlook.com>
2024-07-12 01:21:48 +0300

    reviews

--- PokemonReviewApp/Dto/ReviewDto.cs
+++ PokemonReviewApp/Dto/ReviewDto.cs
@@ -7,6 +7,6 @@ namespace PokemonReviewApp.Dto
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public int Rating { get; set; 
+        public int Rating { get; set; }
     }
 }
